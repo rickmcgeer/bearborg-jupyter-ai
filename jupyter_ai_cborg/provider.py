e88863ff812d4f85f58d0996a2b2e694e6f49f93
awschmeder <awschmeder@lbl.gov>
2025-03-11 11:17:17 -0700

    fix incorrect model strings and remove unusued _astream method

--- jupyter_ai_cborg/provider.py
+++ jupyter_ai_cborg/provider.py
@@ -48,8 +48,8 @@ class CBorgProvider(BaseProvider, OpenAI):
     id = "cborg"
     name = "CBorg"
     models = [
-        "lbl/cborg-coder",
-        "lbl/cborg-deepthought",
+        "lbl/cborg-coder:latest",
+        "lbl/cborg-deepthought:latest",
         "openai/chatgpt:latest",
         "google/gemini:latest",
         "anthropic/claude:latest",
@@ -82,48 +82,13 @@ class CBorgProvider(BaseProvider, OpenAI):
             return error_details.get("code") == "invalid_api_key"
         return False
 
-    @classmethod
-    async def _2astream(
-        self,
-        prompt: str,
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> AsyncIterator[GenerationChunk]:
-        try:
-            params = {**super()._invocation_params, **kwargs, "stream": True}
-        except TypeError:
-            params = {**kwargs, "max_tokens": -1, "stream": True}
-        print("params:", params, [prompt], stop)
-        #OpenAI.get_sub_prompts(self, params, [prompt], stop)  # this mutates params
-        async for stream_resp in await self.async_client.create(
-            prompt=prompt, **params
-        ):
-            if not isinstance(stream_resp, dict):
-                stream_resp = stream_resp.model_dump()
-            chunk = _stream_response_to_generation_chunk(stream_resp)
-    
-            if run_manager:
-                await run_manager.on_llm_new_token(
-                    chunk.text,
-                    chunk=chunk,
-                    verbose=super().verbose,
-                    logprobs=(
-                        chunk.generation_info["logprobs"]
-                        if chunk.generation_info
-                        else None
-                    ),
-                )
-            print("yield chunk c")
-            yield chunk
-
 class CBorgChatProvider(BaseProvider, ChatOpenAI):
     model_config = {"ignored_types": (str,bool,float,Persona,EnvAuthStrategy,CBorgAPIKey)}
     id = "cborg-chat"
     name = "CBorg"
     models = [
-        "lbl/cborg-coder",
-        "lbl/cborg-deepthought",
+        "lbl/cborg-coder:latest",
+        "lbl/cborg-deepthought:latest",
         "openai/chatgpt:latest",
         "google/gemini:latest",
         "anthropic/claude:latest"
@@ -146,7 +111,7 @@ class CBorgChatProvider(BaseProvider, ChatOpenAI):
     @classmethod
     def is_api_key_exc(cls, e: Exception):
         """
-        Determine if the exception is an Blablador API key error.
+        Determine if the exception is an CBorg API key error.
         """
         import openai
 
@@ -154,40 +119,6 @@ class CBorgChatProvider(BaseProvider, ChatOpenAI):
             error_details = e.json_body.get("error", {})
             return error_details.get("code") == "invalid_api_key"
         return False
-
-    @classmethod
-    async def _2astream(
-        self,
-        prompt: str,
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> AsyncIterator[GenerationChunk]:
-        params = {**super()._invocation_params, **kwargs, "stream": True}
-        self.get_sub_prompts(params, [prompt], stop)  # this mutates params
-        async for stream_resp in await self.async_client.create(
-            prompt=prompt, **params
-        ):
-            if not isinstance(stream_resp, dict):
-                stream_resp = stream_resp.model_dump()
-            chunk = _stream_response_to_generation_chunk(stream_resp)
-    
-            if run_manager:
-                await run_manager.on_llm_new_token(
-                    chunk.text,
-                    chunk=chunk,
-                    verbose=super().verbose,
-                    logprobs=(
-                        chunk.generation_info["logprobs"]
-                        if chunk.generation_info
-                        else None
-                    ),
-                )
-            print("yield chunk cc1")
-            yield chunk
-
-
-
 
 class CBorgEmbeddingsProvider(BaseEmbeddingsProvider, OpenAIEmbeddings):
     model_config = {"ignored_types": (str,Persona,EnvAuthStrategy,CBorgAPIKey)}
@@ -207,4 +138,4 @@ class CBorgEmbeddingsProvider(BaseEmbeddingsProvider, OpenAIEmbeddings):
     openai_api_key = CBorgAPIKey()
     openai_api_base = getenv("CBORG_API_ENDPOINT", 'https://api.cborg.lbl.gov') + '/v1'
     openai_organization = "Berkeley Lab"
-    persona = Persona(name="CBorg Jupyternaut", avatar_route="api/ai/static/jupyternaut.svg")
+    persona = Persona(name="CBorg", avatar_route="api/ai/static/jupyternaut.svg")
